9437422da71e641116a877fccf4d67ad7f0c1fba
Faith Duggan <fduggan@nightingale.org>
2017-07-28 13:33:17 -0400

    trail 3

---
+++ Portfolio website/class_starter.js
@@ -0,0 +1,24 @@
+class person 
+{
+	constructor (first_name, last_name, address) 
+	{
+		this.first_name= first_name;
+		this.last_name= last_name;
+		this.address= address;
+	}
+	FullName ()
+	{
+		document.getElementById("myName").innerHTML= this.first_name + this.last_name;
+	}
+	Living ()
+	{
+		document.getElementById("myAddress").innerHTML= this.address
+	}
+
+	// person.first_name = Faith
+	// person.last_name = Duggan
+	// person.address = New York
+}
+
+var Faith = new person("Faith", "Duggan", "New York");
+Faith.FullName();
